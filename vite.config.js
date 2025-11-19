1d560236fb90e623a7918dddca147604621253a8
ShreshthJain0001 <shreshthjain798@gmail.com>
2025-11-19 18:03:48 +0530

    Set base path for Interview-Ai-Assistance-Bot
    Updated base path for deployment configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: "/Inteview-Ai-Assistance-Bot/",
+  // other config...
+}
+
